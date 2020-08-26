b8c636259a8468fbaadd6a0d57b522cf1061147f
reluc <relu.cri@gmail.com>
2020-08-26 19:16:19 +0200

    update explainer example
    Simplify code and enhance TD
    
    Signed-off-by: reluc <relu.cri@gmail.com>

--- material/explainer-code-sample/example-client.js
+++ material/explainer-code-sample/example-client.js
@@ -1,30 +1,14 @@
-Servient = require("@node-wot/core").Servient
-HttpClientFactory = require("@node-wot/binding-http").HttpClientFactory
-
-Helpers = require("@node-wot/core").Helpers
-
-// create Servient and add HTTP binding
-let servient = new Servient();
-servient.addClientFactory(new HttpClientFactory(null));
-
-let wotHelper = new Helpers(servient);
-wotHelper.fetch("http://localhost:8080/Agriculture").then(async (td) => {
-    try {
-        servient.start().then((WoT) => {
-            WoT.consume(td).then((thing) => {
-                // read humidity and temperature sensor
-                thing.readProperty("humidity").then((h) => {
-                    console.log("Humidity: " + h);
-                });
-                thing.readProperty("temperature").then((t) => {
-                    console.log("Temperature: " + t);
-                });
-
-                // subscribe to tooDry event and turn on sprinkler system
-                // ...
-            });
+WoTHelpers.fetch("http://localhost:8080/SoilSensor").then(async (td) => {
+    WoT.consume(td).then((thing) => {
+        // read humidity and temperature sensor
+        thing.readProperty("humidity").then((h) => {
+            console.log("Humidity: " + h);
         });
-    } catch (err) {
-        console.error("Script error:", err);
-    }
+        thing.readProperty("temperature").then((t) => {
+            console.log("Temperature: " + t);
+        });
+        thing.subscribeEvent("tooDry",()=>{
+            // Activate a remote sprinkler
+        })
+    });
 }).catch((err) => { console.error("Fetch error:", err); });
